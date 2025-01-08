48ec560eccc7ea874372fc26ca3a3e13e3832151
Rafael Costa <raf.costa29@gmail.com>
2025-01-08 09:50:32 -0300

    Alterando CORS

--- microservices/api-account/src/main/java/br/com/rcosta/account/config/WebConfig.java
+++ microservices/api-account/src/main/java/br/com/rcosta/account/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permite todas as rotas
-                .allowedOrigins("http://192.168.15.15:8765") // Origem do Gateway/Swagger
+                .allowedOriginPatterns("*") // Origem do Gateway/Swagger
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
                 .allowedHeaders("*") // Cabeçalhos permitidos
                 .allowCredentials(true); // Permite cookies e credenciais
